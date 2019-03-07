13347fb80777aa64b29b65ba3336d9e8df99f710
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-07 11:45:12 -0500

    Added styles

--- src/app.jsx
+++ src/app.jsx
@@ -44,6 +44,7 @@ class Todo extends React.Component {
               <input type="checkbox" checked={this.state.done}
                      onClick={this.handleClick} />
               <input type="text" value={this.state.text}
+                     className={(this.state.done)? 'done':'not-done'}
                      onChange={this.handleChange}
                      onBlur={this.handleSubmit} />
             </span>
