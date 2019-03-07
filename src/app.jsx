fcec86682b0788d6a355fcda357734c4b21a431d
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-06 23:01:45 -0500

    Added a text field and radio button

--- src/app.jsx
+++ src/app.jsx
@@ -1,2 +1,19 @@
-const root = document.getElementById("root");
-ReactDOM.render(<h1>Hello World</h1>, root);
+class Todo extends React.Component{
+    render(){
+        let text = this.props.name;
+        let checked = (this.props.checked == true);
+        // class is reserved keyword on line 1 so in react we use className
+        return <div className="todo">
+            <span>
+                <input type="checkbox" checked="{checked}" />
+                <input type="text" value={text} />
+            </span>
+        </div>;
+    }
+}
+
+const root = document.getElementById('root');
+// Todo is is an instance of class Todo
+ReactDOM.render(
+    <Todo name = "Nikhil" checked = "true" />,
+    root);
